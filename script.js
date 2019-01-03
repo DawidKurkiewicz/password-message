5a94150de11e0bd7d77a1a3135e58be9b3c6e174
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-03 13:02:55 +0100

    added toggle on password colors

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-const input = document.getElementById("pass")
-const div = document.querySelector(".message")
-const password = "user"
-const message = "correct password!"
+const input = document.getElementById("pass");
+const div = document.querySelector(".message");
+const password = "user";
+const message = "correct password!";
 input.addEventListener("keyup", (e) => {
-if( password === e.target.value) {
-    div.innerHTML = message
-}
+    if (password === e.target.value) {
+        div.innerHTML = message;
+        e.target.value = "";
+    } else {
+        div.innerHTML = ""
+    }
 })
+
+input.addEventListener("focus", (e) => {
+e.target.classList.toggle("active")
+})
+
