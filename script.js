443c36c20b1b3fd370afc078096351445a14a6a7
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-03 13:28:17 +0100

    display password using forEach

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 const input = document.getElementById("pass");
 const div = document.querySelector(".message");
-const passwords = ["user1", "user2"]
+const passwords = ["User1", "user2"]
 const messages = ["correct password user1", "correct password user2"]
-input.addEventListener("keyup", (e) => {
-    div.innerHTML = ''
-    const text = e.target.value
-    passwords.forEach((pass, i) => {
-        if (pass === text) {
-            div.innerHTML = messages[i]
-            e.target.value = ''
-        }
-    })
-})
+// input.addEventListener("keyup", (e) => {
+//     div.innerHTML = ''
+//     const text = e.target.value
+//     passwords.forEach((pass, i) => {
+//         if (pass === text) {
+//             div.innerHTML = messages[i]
+//             e.target.value = ''
+//         }
+//     })
+// })
 
 input.addEventListener("focus", (e) => {
     e.target.classList.toggle("active")
 })
 
+
+//ForEach
+
+const showMessage = (e) => {
+    passwords.forEach((pass, i) => {
+        if (pass.toLowerCase() === e.target.value.toLowerCase()) {
+            document.querySelector(".message").innerHTML = messages[i];
+            e.target.value = ''
+
+        }
+    })
+}
+
+input.addEventListener("input", showMessage)
