ed2fa479584b719c0ad88455c711955f264e4187
DawidKurkiewicz <dawidkurkiewicz516@gmail.com>
2019-01-03 15:35:42 +0100

    passwords works using map

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const input = document.getElementById("pass");
 const div = document.querySelector(".message");
 const passwords = ["User1", "user2"]
 const messages = ["correct password user1", "correct password user2"]
+input.addEventListener("focus", (e) => {
+    e.target.classList.toggle("active")
+})
+
 // input.addEventListener("keyup", (e) => {
 //     div.innerHTML = ''
 //     const text = e.target.value
@@ -13,26 +17,32 @@ const messages = ["correct password user1", "correct password user2"]
 //     })
 // })
 
-input.addEventListener("focus", (e) => {
-    e.target.classList.toggle("active")
-})
+//                  ForEach
+// passwords.forEach((pass, i) => {
+//     passwords[i]=pass.toLowerCase()
+// })
+// const showMessage = (e) => {
+//     const input = e.target.value.toLowerCase()
+//     passwords.forEach((pass, i) => {
+//         if (pass === input) {
+//             document.querySelector(".message").innerHTML = messages[i];
+//             e.target.value = ''
+//         }
+//     })
+// }
+// input.addEventListener("input", showMessage)
 
+//                  Map
 
-//ForEach
-
-passwords.forEach((pass, i) => {
-    passwords[i]=pass.toLowerCase()
-})
-
+const lowerCasePass = passwords.map(pass => pass.toLocaleLowerCase())
 const showMessage = (e) => {
-    const input = e.target.value.toLowerCase()
-    passwords.forEach((pass, i) => {
-        if (pass === input) {
-            document.querySelector(".message").innerHTML = messages[i];
+    const input = e.target.value.toLocaleLowerCase()
+    for (let i = 0; i < lowerCasePass.length; i++)
+        if (input == lowerCasePass[i]) {
+            div.innerHTML = messages[i]
             e.target.value = ''
 
         }
-    })
 }
 
 input.addEventListener("input", showMessage)
